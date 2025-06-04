f4b1556a12df0224496d1a921ac4d5280866e30b
Maciej Klepacki <102765127+MaciejKlepacki@users.noreply.github.com>
2025-06-04 19:02:25 +0200

    Time: 3 ms (22.23%), Space: 17.7 MB (88.28%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,5 +1,8 @@
 from math import comb
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        # z dwumianu Newtona
-        return comb(m+n-2, min(m-1, n-1))
+        grid = [[1 for _ in range(n)] for _ in range(m)]
+        for i in range(1, m):
+            for j in range(1, n):
+                grid[i][j] = grid[i-1][j] + grid[i][j-1]
+        return grid[m-1][n-1]
